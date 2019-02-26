22200dccfff727dee909423bf743e83a32fd16a1
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-02-26 14:31:09 +0100

    Added language option.
    JS checks cookies with user language preferences and translate pages.

--- app/scripts/form_options.js
+++ app/scripts/form_options.js
@@ -1,5 +1,3 @@
-const fluidsArray = ['water']
-const materialArray = ['steel']
 const nominalDiameterArray = [8, 10, 15, 20, 25, 32, 40, 50, 65, 80, 100, 125, 150]
 const flowUnitArray = [
     'l/s', 'l/m', 'l/h', 'mm3/s', 'mm3/m', 'mm3/h', 'cm3/s', 'cm3/m', 'cm3/h', 'dm3/s', 'dm3/m', 'dm3/h', 'm3/s', 
@@ -13,10 +11,11 @@ function generateSelectOption(idElement, label, optionsArray) {
     for (option of optionsArray) {
         selectOptions += `<option>${option}</option>`;
     };
+    const translatedLabel = getTranslatedSentence(label);
     let formElement = `
-        <span class="formInputTitle">${label}:</span>
+        <span class="formInputTitle">${translatedLabel}:</span>
         <select id="${idElement}">
-            <optgroup label="${label}">
+            <optgroup label="${translatedLabel}">
             ${selectOptions}
         </select>
     `
@@ -24,51 +23,60 @@ function generateSelectOption(idElement, label, optionsArray) {
 }
 
 function generateInputOption(idElement, label, defaultValue) {
+    const translatedLabel = getTranslatedSentence(label);
     return `
-    <span class="formInputTitle">${label}:</span>
-    <input id="${idElement}" placeholder="${label}" value="${defaultValue}">
+    <span class="formInputTitle">${translatedLabel}:</span>
+    <input id="${idElement}" placeholder="${translatedLabel}" value="${defaultValue}">
     `
 }
 
 function generateHeadlossFlowForm() {
     return `
-        ${generateSelectOption('fluid', 'Fluid', fluidsArray)}
-        ${generateInputOption('temperature', 'Temperature', "")}
-        ${generateSelectOption('material', 'Material', materialArray)}
-        ${generateSelectOption('nominal_diameter', 'Nominal diameter', nominalDiameterArray)}
-        ${generateInputOption('flow', 'Flow', "")}
-        ${generateSelectOption('flow_unit', 'Flow unit', flowUnitArray)}
-        ${generateInputOption('pipe_length', 'Pipe length [m]', "")}
-        ${generateInputOption('roughness', 'Roughness', "1.5")}
-        ${generateInputOption('local_loss_coefficient', 'Local loss coefficient', "0")}
-        ${generateSelectOption('headloss_unit', 'Headloss unit', headlossUnitArray)}
-        <button type="button" id="formButton">Calculate</button>
+        ${generateSelectOption('fluid', "Fluid", getTranslatedSentence("fluidsArrays"))}
+        ${generateInputOption('temperature', "Temperature", "")}
+        ${generateSelectOption('material', "Material", getTranslatedSentence("materialsArrays"))}
+        ${generateSelectOption('nominal_diameter', "NominalDiameter", nominalDiameterArray)}
+        ${generateInputOption('flow', "Flow", "")}
+        ${generateSelectOption('flow_unit', "FlowUnit", flowUnitArray)}
+        ${generateInputOption('pipe_length', "PipeLength", "")}
+        ${generateInputOption('roughness', "Roughness", "1.5")}
+        ${generateInputOption('local_loss_coefficient', "LocalLossCoefficient", "0")}
+        ${generateSelectOption('headloss_unit', "HeadlossUnit", headlossUnitArray)}
+        <button type="button" id="formButton">${getTranslatedSentence("Calculate")}</button>
     `
 }
 
 function generateHeadlossPowerForm() {
     return `
-        ${generateSelectOption('fluid', 'Fluid', fluidsArray)}
-        ${generateInputOption('temperature_supply', 'Temperature supply', "")}
-        ${generateInputOption('temperature_return', 'Temperature return', "")}
-        ${generateSelectOption('material', 'Material', materialArray)}
-        ${generateSelectOption('nominal_diameter', 'Nominal diameter', nominalDiameterArray)}
-        ${generateInputOption('power', 'Power', "")}
-        ${generateSelectOption('power_unit', 'Power unit', powerUnitArray)}
-        ${generateInputOption('pipe_length', 'Pipe length [m]', "")}
-        ${generateInputOption('roughness', 'Roughness', "1.5")}
-        ${generateInputOption('local_loss_coefficient', 'Local loss coefficient', "0")}
-        ${generateSelectOption('headloss_unit', 'Headloss unit', headlossUnitArray)}
-        <button type="button" id="formButton">Calculate</button>
+        ${generateSelectOption('fluid', "Fluid", getTranslatedSentence("fluidsArrays"))}
+        ${generateInputOption('temperature_supply', "TemperatureSupply", "")}
+        ${generateInputOption('temperature_return', "TemperatureReturn", "")}
+        ${generateSelectOption('material', "Material", getTranslatedSentence("materialsArrays"))}
+        ${generateSelectOption('nominal_diameter', "NominalDiameter", nominalDiameterArray)}
+        ${generateInputOption('power', "Power", "")}
+        ${generateSelectOption('power_unit', "PowerUnit", powerUnitArray)}
+        ${generateInputOption('pipe_length', "PipeLength", "")}
+        ${generateInputOption('roughness', "Roughness", "1.5")}
+        ${generateInputOption('local_loss_coefficient', "LocalLossCoefficient", "0")}
+        ${generateSelectOption('headloss_unit', "HeadlossUnit", headlossUnitArray)}
+        <button type="button" id="formButton">${getTranslatedSentence("Calculate")}</button>
     `
 }
 
 function buttonDisable() {
-    formButton.innerHTML = "Waiting";
+    formButton.innerHTML = getTranslatedSentence("Waiting");
 }
 
 function buttonEnable() {
-    formButton.innerHTML = "Calculate";
+    formButton.innerHTML = getTranslatedSentence("Calculate");
 }
 
-calcMode.value = "";
+function createCalcModeForm() {
+    calcMode.innerHTML = `
+    <option value="" hidden>${getTranslatedSentence("ChooseMode")}</option>
+    <option>${getTranslatedSentence("KnownFlow")}</option>
+    <option>${getTranslatedSentence("KnownPower")}</option>
+    `
+    calcMode.value = "";
+}
+
