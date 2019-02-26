9ac94b965b5c439e2abf28ddd3f3dff2713ec47c
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-02-26 15:38:27 +0100

    Added default english language if cookies are disabled.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,9 +2,18 @@ const base_api_url = 'http://localhost:12000'
 const base_frontend_url = 'http://localhost:13000'
 
 
+function getUserCookieLanguage() {
+    try {
+        return document.cookie.split("userLanguage=")[1].split(";")[0];
+    } catch {
+        return 'eng';
+    }
+}
+
 let userLanguage = document.cookie;
 if (!userLanguage) {
     document.cookie = "userLanguage=eng; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-    userLanguage = document.cookie.split("userLanguage=")[1].split(";")[0];
+    userLanguage = getUserCookieLanguage();
 }
-userLanguage = document.cookie.split("userLanguage=")[1].split(";")[0];
+
+userLanguage = getUserCookieLanguage();
